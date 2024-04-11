11c9112b965c95594cd9735fb1905a079bb31631
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2024-04-11 16:07:24 -0300

    Added pathfinding w/ test scene

---
+++ Assets/Scripts/Pathfinding/PathfindMovement.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PathfindMovement : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
