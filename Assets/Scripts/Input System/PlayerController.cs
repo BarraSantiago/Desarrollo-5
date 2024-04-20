e019f33e313555112572f61b577083dd027f8900
Mmonti <matiasmonti2011@hotmail.com>
2024-04-20 12:39:15 -0300

    Add movement & rotate character

---
+++ Assets/Scripts/Input System/PlayerController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+//https://www.youtube.com/watch?v=ud1y5t0sOQc
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float _Speed = 3;
+    [SerializeField] Camera _Camera;
+    PlayerInput _Input;
+    Vector2 _Movement;
+    Vector2 _MousePos;
+
+    Rigidbody2D _Rigidbody;
+
+    private void Awake()
+    {
+        _Input = new PlayerInput();
+        _Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        _Input.Enable();
+
+        _Input.Gameplay.Movement.performed += OnMovement;
+        _Input.Gameplay.Movement.canceled += OnMovement;
+
+        _Input.Gameplay.MousePos.performed += OnMousePos;
+
+    }
+
+    private void OnDisable()
+    {
+        _Input.Gameplay.Movement.performed -= OnMovement;
+        _Input.Gameplay.Movement.canceled -= OnMovement;
+        _Input.Gameplay.MousePos.performed -= OnMousePos;
+
+        _Input.Disable();
+    }
+
+    private void OnMovement(InputAction.CallbackContext context)
+    {
+        _Movement = context.ReadValue<Vector2>();
+    }
+
+    private void OnMousePos(InputAction.CallbackContext context)
+    {
+        _MousePos = _Camera.ScreenToWorldPoint(context.ReadValue<Vector2>());
+    }
+
+    private void FixedUpdate()
+    {
+        _Rigidbody.AddForce(_Movement * _Speed);
+
+        Vector2 facingDirection = _MousePos - _Rigidbody.position;
+        float angle = Mathf.Atan2(facingDirection.y, facingDirection.x) * Mathf.Rad2Deg - 90;
+        _Rigidbody.MoveRotation(angle);
+    }
+}
