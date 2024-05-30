e742c50700101be3a01418197a4a9fd9c3b93d47
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2024-05-30 16:21:35 -0300

    Initial testing

---
+++ Assets/Scripts/DungeonGeneration/RoomTypes.cs
@@ -0,0 +1,12 @@
+namespace DungeonGeneration
+{
+    public enum RoomTypes
+    { 
+        Start = 0,
+        Empty,
+        Enemies,
+        Treasure,
+        Boss,
+        Invalid
+    }
+}
---
+++ Assets/Scripts/DungeonGeneration/SpawnablesIDs.cs
@@ -0,0 +1,16 @@
+namespace DungeonGeneration
+{
+    public enum PropsID
+    { 
+        Bonfire = 0,
+        Treasurechest,
+        Startroomprops,
+        Bossdoor,
+        Invalid
+    }
+
+    public enum BossID
+    {
+        BossBarbariangiant = 0
+    }
+}
