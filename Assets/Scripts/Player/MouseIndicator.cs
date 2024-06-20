c5d18752b836c1b4614e5bbace27f4ff9e610778
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2024-06-20 12:58:02 -0300

    Fix mouse indicator

--- Assets/Scripts/Player/MouseIndicator.cs
+++ Assets/Scripts/Player/MouseIndicator.cs
@@ -1,35 +1,45 @@
 using UnityEngine;
 
-public class MouseIndicator : MonoBehaviour
+namespace player
 {
-    void Update()
+    public class MouseIndicator : MonoBehaviour
     {
-        RotateCombatMouseCircle();
-    }
+        [SerializeField] private float offset = 40;
+        private Camera mainCamera;
+        
+        private void Start()
+        {
+            mainCamera = Camera.main;
+        }
+        
+        void Update()
+        {
+            RotateCombatMouseCircle();
+        }
 
-    public void RotateCombatMouseCircle() 
-    {
-        Vector3 mousePosition = GetMouseWorldPosition();
-        Vector3 playerPosition = transform.position;
+        public void RotateCombatMouseCircle() 
+        {
+            Vector3 mousePosition = GetMouseWorldPosition();
+            Vector3 playerPosition = transform.position;
 
-        Vector3 direction = mousePosition - playerPosition;
-        direction.y = 0f;
+            Vector3 direction = mousePosition - playerPosition;
+            direction.y = 0f;
 
-        float angleRadians = Mathf.Atan2(direction.z, direction.x);
-        float angleDegrees = angleRadians * Mathf.Rad2Deg;
+            float angleRadians = Mathf.Atan2(direction.z, direction.x);
+            float angleDegrees = angleRadians * Mathf.Rad2Deg;
 
-        // Mantener la rotación en X en 90 grados y ajustar solo la rotación en Z
-        transform.rotation = Quaternion.Euler(90, 0, angleDegrees);
-    }
+            transform.rotation = Quaternion.Euler(90, 0, angleDegrees + offset);
+        }
 
-    private Vector3 GetMouseWorldPosition()
-    {
-        Vector3 mouseScreenPosition = Input.mousePosition;
-        Ray ray = Camera.main.ScreenPointToRay(mouseScreenPosition);
-        RaycastHit hit;
+        private Vector3 GetMouseWorldPosition()
+        {
+            Vector3 mouseScreenPosition = Input.mousePosition;
+            Ray ray = mainCamera.ScreenPointToRay(mouseScreenPosition);
+            RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit)) return hit.point;
+            if (Physics.Raycast(ray, out hit)) return hit.point;
 
-        return Vector3.zero;
+            return Vector3.zero;
+        }
     }
 }
