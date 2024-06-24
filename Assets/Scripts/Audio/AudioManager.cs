f8cea473701968c543942b65132c4a3d9baa1db4
Mmonti <matiasmonti2011@hotmail.com>
2024-06-24 16:09:23 -0300

    Add AudioManager

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [System.Serializable]
+    public class Sound
+    {
+        public string name;
+        public AudioClip clip;
+        public float volume = 1.0f;
+        public float pitch = 1.0f;
+        public bool loop = false;
+    }
+
+    [SerializeField] private Sound[] sounds;
+
+    private Dictionary<string, AudioSource> soundDictionary = new Dictionary<string, AudioSource>();
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        foreach (Sound sound in sounds)
+        {
+            AudioSource source = gameObject.AddComponent<AudioSource>();
+            source.clip = sound.clip;
+            source.volume = sound.volume;
+            source.pitch = sound.pitch;
+            source.loop = sound.loop;
+            soundDictionary[sound.name] = source;
+        }
+    }
+
+    public void Play(string name)
+    {
+        if (soundDictionary.ContainsKey(name))
+        {
+            soundDictionary[name].Play();
+        }
+        else
+        {
+            Debug.LogWarning("Sound not found: " + name);
+        }
+    }
+
+    public void Stop(string name)
+    {
+        if (soundDictionary.ContainsKey(name))
+        {
+            soundDictionary[name].Stop();
+        }
+        else
+        {
+            Debug.LogWarning("Sound not found: " + name);
+        }
+    }
+}
