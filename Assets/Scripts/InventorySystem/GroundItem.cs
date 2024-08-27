da1b755ccbf9406d83b9fd5dc0cf6e5f9ca45d7c
Santi <barrasanti@gmail.com>
2024-08-27 19:53:33 -0300

    Add grab item with mouse

--- Assets/Scripts/InventorySystem/GroundItem.cs
+++ Assets/Scripts/InventorySystem/GroundItem.cs
@@ -1,12 +1,29 @@
-﻿using UnityEngine;
+﻿using Interactable;
+using player;
+using UnityEngine;
 
 namespace InventorySystem
 {
-    public class GroundItem : MonoBehaviour
+    public class GroundItem : MonoBehaviour, IInteractable
     {
         public ItemObject item;
         public int amount = 1;
         public bool droppedByPlayer = false;
         public float droppedTime;
+        public bool State { get; }
+        public bool Interact()
+        {
+            Player player = FindObjectOfType<Player>();
+            
+            if (player is null || !player.inventory.AddItem(new Item(item), amount)) return false;
+            
+            Destroy(gameObject);
+            return true;
+        }
+
+        public void Close()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
