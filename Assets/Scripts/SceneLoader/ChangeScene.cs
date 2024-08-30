e0723d877fb27c26bd230d1d785ceb2dd7a2b512
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2024-08-30 19:27:12 -0300

    Fix bugs

---
+++ Assets/Scripts/SceneLoader/ChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SceneLoader
+{
+    public class ChangeScene : MonoBehaviour
+    {
+        [SerializeField] private string sceneName;
+
+        public void LoadScene()
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
