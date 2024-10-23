8a57421ffba42e2d7a5495f2f25b9b4c13a2795d
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2024-10-22 21:20:08 -0300

    Craft update WIP

--- Assets/Scripts/Store/Shops/Shop.cs
+++ Assets/Scripts/Store/Shops/Shop.cs
@@ -20,11 +20,13 @@ namespace Store.Shops
         [SerializeField] private Button upgradeButton;
         [SerializeField] private Player player;
         [SerializeField] private TMP_Text upgradeText;
+        [SerializeField] private bool isCraftingShop;
 
         [Header("Item setup")] 
         [SerializeField] private GameObject shopItemPrefab;
         [SerializeField] private Image selectedItemImage;
         [SerializeField] private Transform shopItemsParent;
+        [SerializeField] private TMP_InputField inputField;
         [SerializeField] private Button buyButton;
         [SerializeField] private Button increaseAmountButton;
         [SerializeField] private Button decreaseAmountButton;
@@ -43,10 +45,38 @@ namespace Store.Shops
         private int _shopMaxLevel;
         private int _shopUpgradeCost;
         private int _shopUpgradeCostMultiplier;
-        private int _currentCost;
-        private int _currentAmount = 1;
-        private ItemObject _selectedItem;
 
+        private int CurrentCost
+        {
+            get => _currentCost;
+            set
+            {
+                _currentCost = value;
+                UpdateTextColor();
+                costText.text = _currentCost.ToString();
+            }
+        }
+
+        private int _currentAmount = 1;
+        private float _craftChance = 0;
+
+        private float CraftChanceMultiplier
+        {
+            get => _craftChanceMultiplier;
+            set
+            {
+                _craftChanceMultiplier = value;
+                UpdateTextColor();
+                craftChanceText.text = _craftChance * _craftChanceMultiplier + "%";
+            }
+        }
+
+        private ItemObject _selectedItem;
+        private int _currentCost;
+        private float _craftChanceMultiplier = 1;
+        private const int MaxAmount = 999999;
+        private const int MinAmount = 1;
+        
         #endregion
 
 
@@ -57,13 +87,10 @@ namespace Store.Shops
             _shopUpgradeCost = 150;
             _shopUpgradeCostMultiplier = 5;
             _shopUpgradeCost *= _shopUpgradeCostMultiplier * _shopLevel;
-            if(amountText) amountText.text = _currentAmount.ToString();
-            if(upgradeText) upgradeText.text = _shopUpgradeCost.ToString();
-
-            upgradeButton?.onClick.AddListener(UpgradeShop);
-            buyButton.onClick.AddListener(BuyItem);
-            increaseAmountButton?.onClick.AddListener(IncreaseAmount);
-            decreaseAmountButton?.onClick.AddListener(DecreaseAmount);
+            if (amountText) amountText.text = _currentAmount.ToString();
+            if (upgradeText) upgradeText.text = _shopUpgradeCost.ToString();
+            
+            
             ListItems();
 
             UpdateAvailability(player.money);
@@ -71,7 +98,7 @@ namespace Store.Shops
             selectedItemImage.preserveAspect = true;
             SelectItem(databases[0].ItemObjects[0].data.id);
         }
-
+        
         private void OnEnable()
         {
             UpdateAvailability(player.money);
@@ -81,12 +108,13 @@ namespace Store.Shops
         {
             _selectedItem = completeDatabase.ItemObjects[itemId];
             selectedItemImage.sprite = _selectedItem.uiDisplay;
-            
-            
+
+
             int recipeItemsLength = _selectedItem.data.recipe?.items?.Length ?? 0;
 
             if (_selectedItem.data.craftable && _selectedItem.data.recipe)
             {
+
                 for (int i = 0; i < shopRecipes.Length; i++)
                 {
                     shopRecipes[i].gameObject.SetActive(i < recipeItemsLength);
@@ -98,44 +126,60 @@ namespace Store.Shops
                     shopRecipes[i].SetRecipe(itemRecipe, _selectedItem.data.recipe.items[i].amount,
                         player.inventory.GetItemCount(_selectedItem.data));
                 }
-                craftChanceText.text = _selectedItem.data.recipe.craftChance.ToString(CultureInfo.CurrentCulture) + "%";
-                _currentCost = _selectedItem.data.listPrice.originalPrice / 2;
+
+                _craftChance = completeDatabase.ItemObjects[_selectedItem.data.id].data.recipe.craftChance;
+                craftChanceText.text = (_craftChance * _craftChanceMultiplier).ToString(CultureInfo.CurrentCulture) + "%";
+                CurrentCost = _selectedItem.data.listPrice.originalPrice / 3;
+                inputField.text = 0.ToString();
+                CraftChanceMultiplier = 1;
+                
             }
             else
             {
-                _currentCost = (int)(_selectedItem.data.listPrice.originalPrice * itemCostMultiplier * _currentAmount);
+                CurrentCost = (int)(_selectedItem.data.listPrice.originalPrice * itemCostMultiplier * _currentAmount);
             }
-
-            UpdateTextColor();
-            costText.text = _currentCost.ToString();
         }
 
         private void BuyItem()
         {
-            if (player.money < _currentCost)
+            if (player.money < CurrentCost)
             {
                 return;
             }
 
-            switch (_selectedItem.data.craftable)
+            if (_selectedItem.data.craftable)
             {
-                case true when !CheckRecipe():
-                    return;
-                case true:
-                {
-                    foreach (var itemEntry in _selectedItem.data.recipe.items)
-                    {
-                        ItemObject currentEntry = completeDatabase.ItemObjects[itemEntry.itemID];
-                        player.inventory.RemoveItem(currentEntry.data, itemEntry.amount);
-                    }
-
-                    break;
-                }
+                if (!CraftItem()) return;
+            }
+            else
+            {
+                player.money -= CurrentCost;
+                player.inventory.AddItem(_selectedItem.data, _currentAmount);
             }
             
-            player.money -= _currentCost;
-            player.inventory.AddItem(_selectedItem.data, _currentAmount);
             UpdateAvailability(player.money);
+        }
+
+        private bool CraftItem()
+        {
+            if (!CheckRecipe()) return false;
+            foreach (var itemEntry in _selectedItem.data.recipe.items)
+            {
+                ItemObject currentEntry = completeDatabase.ItemObjects[itemEntry.itemID];
+                player.inventory.RemoveItem(currentEntry.data, itemEntry.amount);
+            }
+            
+            if (Random.Range(0, 100) < _craftChance * CraftChanceMultiplier)
+            {
+                player.inventory.AddItem(_selectedItem.data, _currentAmount);
+                AudioManager.instance.Play("CraftingSuccess");
+            }
+            else
+            {
+                AudioManager.instance.Play("CraftingFail");
+            }
+            
+            return true;
         }
 
         private bool CheckRecipe()
@@ -145,13 +189,52 @@ namespace Store.Shops
                 where player.inventory.GetItemCount(currentEntry.data) < itemEntry.amount
                 select itemEntry).Any();
         }
+        
+        private void ChangeCraftChanceMod()
+        {
+            if (int.TryParse(inputField.text, out int newPrice))
+            {
+                inputField.text = newPrice switch
+                {
+                    < MinAmount => MinAmount.ToString(),
+                    > MaxAmount => MaxAmount.ToString(),
+                    _ => "$" + inputField.text
+                };
+                
+                CurrentCost = _selectedItem.data.listPrice.originalPrice / 3 + newPrice;
+                CraftChanceMultiplier = ((float)newPrice / _selectedItem.data.listPrice.originalPrice) + 1;
+
+                if (!(CraftChanceMultiplier * _selectedItem.data.recipe.craftChance > 100)) return;
+
+                SetMaxCraftChance();
+            }
+            else
+            {
+                inputField.text = 0.ToString();
+                CraftChanceMultiplier = 1;
+                CurrentCost = _selectedItem.data.listPrice.originalPrice / 3;
+            }
+        }
+
+        private void SetMaxCraftChance()
+        {
+            CraftChanceMultiplier = 100 / _selectedItem.data.recipe.craftChance;
+            inputField.text = CraftChanceMultiplier.ToString(CultureInfo.CurrentCulture);
+            CurrentCost = _selectedItem.data.listPrice.originalPrice / 3 + (int)(_selectedItem.data.listPrice.originalPrice * CraftChanceMultiplier);
+        }
+
 
         private void UpdateAvailability(int money)
         {
-           UpdateTextColor();
+            UpdateTextColor();
 
-           CheckLevel();
+            CheckLevel();
 
+            CheckCraftingMaterials();
+        }
+
+        private void CheckCraftingMaterials(int slotIndex = -1)
+        {
             if (!_selectedItem || !_selectedItem.data.craftable) return;
 
             for (int i = 0; i < _selectedItem.data.recipe.items.Length; i++)
@@ -164,17 +247,17 @@ namespace Store.Shops
 
         private void UpdateTextColor()
         {
-            costText.color = player.money < _currentCost ? Color.red : Color.green;
-            if(upgradeText) upgradeText.color = player.money < _shopUpgradeCost ? Color.red : Color.green;
+            costText.color = player.money < CurrentCost ? Color.red : Color.green;
+            if (upgradeText) upgradeText.color = player.money < _shopUpgradeCost ? Color.red : Color.green;
         }
 
         private void CheckLevel()
         {
             if (_shopLevel < _shopMaxLevel) return;
-            
-            if(upgradeButton) upgradeButton.interactable = false;
-            if(upgradeText) upgradeText.text = "Max Level";
-            if(upgradeText) upgradeText.color = Color.gray;
+
+            if (upgradeButton) upgradeButton.interactable = false;
+            if (upgradeText) upgradeText.text = "Max Level";
+            if (upgradeText) upgradeText.color = Color.gray;
         }
 
         private void UpgradeShop()
@@ -210,34 +293,47 @@ namespace Store.Shops
                 }
             }
         }
-        
+
         private void DecreaseAmount()
         {
             if (_currentAmount <= 1) return;
             _currentAmount--;
             amountText.text = _currentAmount.ToString();
-            _currentCost = (int)(_selectedItem.data.listPrice.originalPrice * itemCostMultiplier * _currentAmount);
-            UpdateTextColor();
-            costText.text = _currentCost.ToString();
+            CurrentCost = (int)(_selectedItem.data.listPrice.originalPrice * itemCostMultiplier * _currentAmount);
         }
 
         private void IncreaseAmount()
         {
             _currentAmount++;
             amountText.text = _currentAmount.ToString();
-            _currentCost = (int)(_selectedItem.data.listPrice.originalPrice * itemCostMultiplier * _currentAmount);
-            UpdateTextColor();
-            costText.text = _currentCost.ToString();
+            CurrentCost = (int)(_selectedItem.data.listPrice.originalPrice * itemCostMultiplier * _currentAmount);
         }
 
+        private void OnSelectInput()
+        {
+            inputField.text = inputField.text.Replace("$", "");
+        }
+        
         public void Initialize()
         {
             foreach (var shopItem in _shopItems)
             {
                 shopItem.OnSelectItem += SelectItem;
             }
+
             Player.OnMoneyUpdate += UpdateAvailability;
+
+            player.inventory.OnItemAdded += CheckCraftingMaterials;
+            upgradeButton?.onClick.AddListener(UpgradeShop);
+            buyButton.onClick.AddListener(BuyItem);
+            increaseAmountButton?.onClick.AddListener(IncreaseAmount);
+            decreaseAmountButton?.onClick.AddListener(DecreaseAmount);
+
+            if (!isCraftingShop) return;
             
+            inputField.onEndEdit.AddListener(delegate { ChangeCraftChanceMod(); });
+            inputField.onDeselect.AddListener(delegate { ChangeCraftChanceMod(); });
+            inputField.onSelect.AddListener(delegate { OnSelectInput(); });
         }
 
         public void Deinitialize()
@@ -246,7 +342,19 @@ namespace Store.Shops
             {
                 shopItem.OnSelectItem -= SelectItem;
             }
+
             Player.OnMoneyUpdate -= UpdateAvailability;
-         }
+            player.inventory.OnItemAdded -= CheckCraftingMaterials;
+
+            upgradeButton?.onClick.RemoveListener(UpgradeShop);
+            buyButton.onClick.RemoveListener(BuyItem);
+            increaseAmountButton?.onClick.RemoveListener(IncreaseAmount);
+            decreaseAmountButton?.onClick.RemoveListener(DecreaseAmount);
+            
+            if (!isCraftingShop) return;
+            inputField.onEndEdit.RemoveListener(delegate { ChangeCraftChanceMod(); });
+            inputField.onDeselect.RemoveListener(delegate { ChangeCraftChanceMod(); });
+            inputField.onSelect.RemoveListener(delegate { OnSelectInput(); });
+        }
     }
 }
