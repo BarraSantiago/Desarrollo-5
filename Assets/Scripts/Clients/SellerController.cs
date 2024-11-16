e8ffa3edccbc4de98cee8dc19e24b495022428ef
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2024-11-16 13:28:03 -0300

    Add new assets

---
+++ Assets/Scripts/Clients/SellerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Clients
+{
+    public class SellerController : MonoBehaviour
+    {
+        [SerializeField] private Animator sellerAnimator;
+        [SerializeField] private Animator chestAnimator;
+
+        private readonly int _charge = Animator.StringToHash("Charge");
+        private readonly int _open = Animator.StringToHash("Open");
+
+        private void Awake()
+        {
+            Client.OnItemBought += OpenChest;
+        }
+
+        private void OnDestroy()
+        {
+            Client.OnItemBought -= OpenChest;
+        }
+
+        private void OpenChest(int i, int i1)
+        {
+            sellerAnimator.SetTrigger(_charge);
+            chestAnimator.SetTrigger(_open);
+        }
+    }
+}
