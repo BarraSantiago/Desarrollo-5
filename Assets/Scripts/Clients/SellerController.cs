f2ea6114209a36c8ef94fa802c5e3c57a0ff6ce4
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2024-11-18 13:15:01 -0300

    Update SellerController.cs

--- Assets/Scripts/Clients/SellerController.cs
+++ Assets/Scripts/Clients/SellerController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using Store;
+using UnityEngine;
 
 namespace Clients
 {
@@ -6,7 +8,62 @@ namespace Clients
     {
         [SerializeField] private Animator sellerAnimator;
         [SerializeField] private Animator chestAnimator;
+        [SerializeField] private Transform chestTop;
+        [SerializeField] private Transform chestMid;
+        [SerializeField] private Transform chestBot;
 
+        private void Start()
+        {
+            StoreManager.OnMoneyUpdated += UpdateMoney;
+        }
+
+        private void UpdateMoney(int currentMoney, int maxMoney)
+        {
+            _currentMoney = currentMoney;
+            MaxMoney = maxMoney;
+        }
+
+        public int MaxMoney
+        {
+            get => _maxMoney;
+            set
+            {
+                _maxMoney = value;
+                UpdateChestLevel();
+            }
+        }
+
+        private void UpdateChestLevel()
+        {
+            int level = _currentMoney * 100 / _maxMoney;
+            
+            switch (level)
+            {
+                case > 90:
+                    chestTop.gameObject.SetActive(true);
+                    chestMid.gameObject.SetActive(false);
+                    chestBot.gameObject.SetActive(false);
+                    break;
+                case > 50:
+                    chestTop.gameObject.SetActive(false);
+                    chestMid.gameObject.SetActive(true);
+                    chestBot.gameObject.SetActive(false);
+                    break;
+                case > 25:
+                    chestTop.gameObject.SetActive(false);
+                    chestMid.gameObject.SetActive(false);
+                    chestBot.gameObject.SetActive(true);
+                    break;
+                default:
+                    chestTop.gameObject.SetActive(false);
+                    chestMid.gameObject.SetActive(false);
+                    chestBot.gameObject.SetActive(false);
+                    break;
+            }
+        }
+
+        private int _maxMoney;
+        private int _currentMoney;
         private readonly int _charge = Animator.StringToHash("Charge");
         private readonly int _open = Animator.StringToHash("Open");
 
