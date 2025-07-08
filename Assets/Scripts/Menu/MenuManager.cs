fc0aa434a85ed5b45347fa3f42e52dd5ced10d51
BarraSantiago <barrasanti@gmail.com>
2025-07-08 16:51:20 -0300

    Add new game

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -1,19 +1,64 @@
+using System;
 using TMPro;
-
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Utils;
 
 namespace Menu
 {
     public class MenuManager : MonoBehaviour
     {
         [SerializeField] private TMP_Text versionText = null;
+        [SerializeField] private Button continueButton = null;
+        [SerializeField] private Button newGameButton = null;
+        [SerializeField] private Button confirmationButton = null;
+        [SerializeField] private GameObject confirmationWindow = null;
         private const string SceneName = "LoadingScene";
-        
+
         public void Start()
         {
-            if (versionText != null) versionText.text = "Ver " + Application.version;
+            if (versionText) versionText.text = "Ver " + Application.version;
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            OnSceneLoaded(SceneManager.GetActiveScene(), LoadSceneMode.Single);
         }
+        
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            continueButton.interactable = SaveFileManager.HasSavedFiles();
+            confirmationWindow.SetActive(false);
+            if (SaveFileManager.HasSavedFiles())
+            {
+                continueButton.onClick.AddListener(() =>
+                {
+                    SceneManager.LoadScene(SceneName);
+                });
+                newGameButton.onClick.AddListener(() =>
+                {
+                    confirmationWindow.SetActive(true);
+                });
+                confirmationButton.onClick.AddListener(() =>
+                {
+                    PlayerPrefs.SetInt("PopularityXp", 0);
+                    PlayerPrefs.SetInt("PopularityLevel", 0);
+                    PlayerPrefs.SetInt("ShopLevel", 1);
+                    PlayerPrefs.SetInt("HasInteracted", 0);
+                    PlayerPrefs.SetInt("PlayerMoney", 250);
+                    PlayerPrefs.SetInt("TutorialAccessed", 0);
+                    PlayerPrefs.Save();
+                    SaveFileManager.DeleteAllSaves();
+                    SceneManager.LoadScene(SceneName);
+                });
+            }
+            else
+            {
+                newGameButton.onClick.AddListener(() =>
+                {
+                    SceneManager.LoadScene(SceneName);
+                });
+            }
+        }
+
         /// <summary>
         /// Quits the application
         /// </summary>
@@ -22,14 +67,6 @@ namespace Menu
             Application.Quit();
         }
 
-        /// <summary>
-        /// Loads the scene saved in SceneName.
-        /// </summary>
-        public void LoadSceneByName()
-        {
-            SceneManager.LoadScene(SceneName);
-        }
-        
         /// <summary>
         /// Loads the required scene by the Id introduced.
         /// </summary>
@@ -41,6 +78,11 @@ namespace Menu
         public void OpenMenu(GameObject menu)
         {
             menu.SetActive(!menu.activeSelf);
+        }
+
+        private void OnDestroy()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
         }
     }
 }
---
+++ Assets/Scripts/Utils/SaveFileManager.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using UnityEngine;
+namespace Utils
+{
+
+    public static class SaveFileManager
+    {
+        // Checks if there are any files in the persistent data path
+        public static bool HasSavedFiles()
+        {
+            string path = Application.persistentDataPath;
+            if (!Directory.Exists(path)) return false;
+            string[] files = Directory.GetFiles(path);
+            return files.Length > 0;
+        }
+
+        // Deletes all files in the persistent data path
+        public static void DeleteAllSaves()
+        {
+            string path = Application.persistentDataPath;
+            if (!Directory.Exists(path)) return;
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                File.Delete(file);
+            }
+        }
+    }
+}
